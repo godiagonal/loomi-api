01eea7d418c089a910bcd1848d0044db7dd5ba3a
Samuel Johansson <samueljohanssonhue@gmail.com>
2019-04-20 15:14:41 +0200

    Add recipe endpoints for read, update and delete

--- app/recipes/controller.ts
+++ app/recipes/controller.ts
@@ -2,17 +2,64 @@ import { Router } from 'express';
 import { Recipe } from './model';
 
 const controller = Router()
-  .get('/', (req, res) => {
-    Recipe.find()
-      .then(items => res.json(items))
-      .catch(err => res.status(404).json(err));
+  .get('/', async (req, res) => {
+    try {
+      const recipes = await Recipe.find();
+      res.json(recipes);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   })
-  .post('/', (req, res) => {
-    const newRecipe = new Recipe(req.body);
-    newRecipe
-      .save()
-      .then(recipe => res.json(recipe))
-      .catch(err => res.status(400).json(err));
+  .post('/', async (req, res) => {
+    try {
+      const recipe = await new Recipe(req.body).save();
+      res.json(recipe);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  })
+  .get('/:id', async (req, res) => {
+    try {
+      const recipe = await Recipe.findOne({ _id: req.params.id });
+      if (recipe == null) {
+        res.status(404).json({ message: 'Recipe not found.' });
+      } else {
+        res.json(recipe);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+    Recipe.findOne({ _id: req.params.id })
+      .then(recipe => {})
+      .catch(err => res.status(500).json(err));
+  })
+  .patch('/:id', async (req, res) => {
+    try {
+      const info: any = await Recipe.updateOne(
+        { _id: req.params.id },
+        { ...req.body, updated: Date.now() }
+      );
+      if (info.nModified === 0) {
+        res.status(404).json({ message: 'Recipe not found.' });
+      } else {
+        const recipe = await Recipe.findOne({ _id: req.params.id });
+        res.json(recipe);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  })
+  .delete('/:id', async (req, res) => {
+    try {
+      const info: any = await Recipe.deleteOne({ _id: req.params.id });
+      if (info.deletedCount === 0) {
+        res.status(404).json({ message: 'Recipe not found.' });
+      } else {
+        res.json({});
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
   });
 
 export default controller;
