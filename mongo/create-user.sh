dc89447e96636eade23765533339bc2a5d8c168a
Samuel Johansson <samueljohanssonhue@gmail.com>
2019-04-23 23:40:12 +0200

    Make mongo init scripts use environment variables passed from docker instead of parsing the .env file

--- mongo/create-user.sh
+++ mongo/create-user.sh
@@ -1,6 +1,3 @@
-# Import environment variables from .env
-export $(egrep -v '^#' .env | xargs)
-
 echo "Creating mongo user '$DB_USER'..."
-mongo admin --host localhost -u $DB_ROOT_USER -p $DB_ROOT_PWD --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles: [{role: 'readWrite', db: '$DB_NAME'}]});"
+mongo admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db.createUser({user: '$DB_USER', pwd: '$DB_PWD', roles: [{role: 'readWrite', db: '$DB_NAME'}]});"
 echo "Mongo user '$DB_USER' created."
